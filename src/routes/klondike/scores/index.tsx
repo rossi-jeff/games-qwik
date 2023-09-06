f68c8a5311e1af56261a39f72ec28c92a1b76137
Jeff Rossi <jrossi@intouchgps.com>
2023-09-06 15:18:15 -0400

    klondike, poker square

--- src/routes/klondike/scores/index.tsx
+++ src/routes/klondike/scores/index.tsx
@@ -1,9 +1,51 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useTask$, $ } from '@builder.io/qwik'
+import type { Klondike } from '../../../types/klondike.type'
+import { RestClient } from '../../../lib/rest-client'
 
 export default component$(() => {
-  return (
-    <div>
-      New route works.
-    </div>
-  );
-});
+	const path = 'api/klondike'
+	const count = useSignal(0)
+	const limit = useSignal(10)
+	const offset = useSignal(0)
+	const items = useSignal<Klondike[]>([])
+
+	const loadData = $(
+		async (path: string, params?: { [key: string]: number }) => {
+			const client = new RestClient()
+			const req = await client.get({ path, params })
+			return await req.json()
+		}
+	)
+
+	useTask$(async () => {
+		const params = { Limit: limit.value, Offset: offset.value }
+		const data = await loadData(path, params)
+		count.value = data.Count
+		limit.value = data.Limit
+		offset.value = data.Offset
+		items.value = data.Items
+	})
+	return (
+		<div>
+			<h1>Klondike Scores</h1>
+			<div class="score-list">
+				<div class="score-header">
+					<div class="cell-48-left">User</div>
+					<div class="cell-20-center">Status</div>
+					<div class="cell-20-center">Time</div>
+					<div class="cell-20-right">Moves</div>
+				</div>
+				{items.value.map((k) => (
+					<div key={k.id} class="score-row">
+						<div class="cell-48-left">
+							{k.user ? k.user.UserName : 'Anonymous'}
+						</div>
+						<div class="cell-20-center">{k.Status}</div>
+						<div class="cell-20-center">{k.Elapsed}</div>
+						<div class="cell-20-right">{k.Moves}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+})
--- src/routes/poker_square/scores/index.tsx
+++ src/routes/poker_square/scores/index.tsx
@@ -1,9 +1,49 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useTask$, $ } from '@builder.io/qwik'
+import type { PokerSquare } from '../../../types/poker-square.type'
+import { RestClient } from '../../../lib/rest-client'
 
 export default component$(() => {
-  return (
-    <div>
-      New route works.
-    </div>
-  );
-});
+	const path = 'api/poker_square'
+	const count = useSignal(0)
+	const limit = useSignal(10)
+	const offset = useSignal(0)
+	const items = useSignal<PokerSquare[]>([])
+
+	const loadData = $(
+		async (path: string, params?: { [key: string]: number }) => {
+			const client = new RestClient()
+			const req = await client.get({ path, params })
+			return await req.json()
+		}
+	)
+
+	useTask$(async () => {
+		const params = { Limit: limit.value, Offset: offset.value }
+		const data = await loadData(path, params)
+		count.value = data.Count
+		limit.value = data.Limit
+		offset.value = data.Offset
+		items.value = data.Items
+	})
+	return (
+		<div>
+			<h1>Poker Square Scores</h1>
+			<div class="score-list">
+				<div class="score-header">
+					<div class="cell-48-left">User</div>
+					<div class="cell-20-center">Status</div>
+					<div class="cell-20-right">Score</div>
+				</div>
+				{items.value.map((ps) => (
+					<div key={ps.id} class="score-row">
+						<div class="cell-48-left">
+							{ps.user ? ps.user.UserName : 'Anonymous'}
+						</div>
+						<div class="cell-20-center">{ps.Status}</div>
+						<div class="cell-20-right">{ps.Score}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+})
