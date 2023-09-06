b4d2f4c5c14bee1d19faea9b2be5d6d22a19b519
Jeff Rossi <jrossi@intouchgps.com>
2023-09-06 13:58:19 -0400

    free cell, guess word, hang man

--- src/routes/free_cell/scores/index.tsx
+++ src/routes/free_cell/scores/index.tsx
@@ -1,9 +1,51 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useTask$, $ } from '@builder.io/qwik'
+import type { FreeCell } from '../../../types/free-cell.type'
+import { RestClient } from '../../../lib/rest-client'
 
 export default component$(() => {
-  return (
-    <div>
-      New route works.
-    </div>
-  );
-});
+	const path = 'api/free_cell'
+	const count = useSignal(0)
+	const limit = useSignal(10)
+	const offset = useSignal(0)
+	const items = useSignal<FreeCell[]>([])
+
+	const loadData = $(
+		async (path: string, params?: { [key: string]: number }) => {
+			const client = new RestClient()
+			const req = await client.get({ path, params })
+			return await req.json()
+		}
+	)
+
+	useTask$(async () => {
+		const params = { Limit: limit.value, Offset: offset.value }
+		const data = await loadData(path, params)
+		count.value = data.Count
+		limit.value = data.Limit
+		offset.value = data.Offset
+		items.value = data.Items
+	})
+	return (
+		<div>
+			<h1>Free Cell Scores</h1>
+			<div class="score-list">
+				<div class="score-header">
+					<div class="cell-48-left">User</div>
+					<div class="cell-20-center">Status</div>
+					<div class="cell-20-center">Time</div>
+					<div class="cell-20-right">Moves</div>
+				</div>
+				{items.value.map((f) => (
+					<div key={f.id} class="score-row">
+						<div class="cell-48-left">
+							{f.user ? f.user.UserName : 'Anonymous'}
+						</div>
+						<div class="cell-20-center">{f.Status}</div>
+						<div class="cell-20-center">{f.Elapsed}</div>
+						<div class="cell-20-right">{f.Moves}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+})
