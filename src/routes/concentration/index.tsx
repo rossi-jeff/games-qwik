a0df878fac37cb711e7bfa94f9de77433ca33cbe
Jeff Rossi <rossi.jeff@gmail.com>
2023-09-06 11:28:23 -0400

    nav skeleton

---
+++ src/routes/concentration/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <div>
+      Concentration works.
+    </div>
+  );
+});
---
+++ src/routes/hang_man/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <div>
+      Hang Man works.
+    </div>
+  );
+});
