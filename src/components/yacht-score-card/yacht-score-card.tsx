883212356aafb30773aef535d64ce578a472228f
Jeff Rossi <jrossi@intouchgps.com>
2023-09-09 09:15:12 -0400

    die graphic, css

--- src/components/yacht-score-card/yacht-score-card.tsx
+++ src/components/yacht-score-card/yacht-score-card.tsx
@@ -1,25 +1,25 @@
-import { component$ } from "@builder.io/qwik";
-import type { YachtTurn } from "~/types/yacht-turn.type";
+import { component$ } from '@builder.io/qwik'
+import type { YachtTurn } from '~/types/yacht-turn.type'
 
 export interface YachtScoreCardProps {
-  total: number;
-  turns: YachtTurn[];
+	total: number
+	turns: YachtTurn[]
 }
 
 export const YachtScoreCard = component$<YachtScoreCardProps>((props) => {
-  const { total, turns } = props;
-  return (
-    <div>
-      {turns.map((t, i) => (
-        <div key={i} class="flex flex-wrap">
-          <div class="flex-grow">{t.Category}</div>
-          <div class="w-10 text-right">{t.Score}</div>
-        </div>
-      ))}
-      <div class="flex flex-wrap">
-        <div class="flex-grow text-right">Total:</div>
-        <div class="w-10 text-right">{total}</div>
-      </div>
-    </div>
-  );
-});
+	const { total, turns } = props
+	return (
+		<div class="yacht-score-card">
+			{turns.map((t, i) => (
+				<div key={i} class="yacht-score-card-row">
+					<div class="flex-grow">{t.Category}</div>
+					<div class="w-10 text-right">{t.Score}</div>
+				</div>
+			))}
+			<div class="yacht-score-card-row">
+				<div class="flex-grow text-right">Total:</div>
+				<div class="w-10 text-right">{total}</div>
+			</div>
+		</div>
+	)
+})
--- src/components/yacht-turn-display/yacht-turn-display.tsx
+++ src/components/yacht-turn-display/yacht-turn-display.tsx
@@ -1,73 +1,76 @@
 import {
-  type QRL,
-  component$,
-  $,
-  useSignal,
-  useTask$,
-  type QwikChangeEvent,
-} from "@builder.io/qwik";
+	type QRL,
+	component$,
+	$,
+	useSignal,
+	useTask$,
+	type QwikChangeEvent,
+} from '@builder.io/qwik'
+import { DieDisplayLg } from '../die-display-lg/die-display-lg'
 
 export interface YachtTurnDisplayProps {
-  roll: string;
-  heading: string;
-  btnLabel: string;
-  rollDice: QRL<(keep: number[]) => void>;
-  disabled: boolean;
+	roll: string
+	heading: string
+	btnLabel: string
+	rollDice: QRL<(keep: number[]) => void>
+	disabled: boolean
 }
 
 export const YachtTurnDisplay = component$<YachtTurnDisplayProps>((props) => {
-  const { heading, btnLabel, disabled } = props;
-  const keep = useSignal<number[]>([]);
-  const dice = useSignal<number[]>([]);
-  const checked = useSignal<number[]>([]);
+	const { heading, btnLabel, disabled } = props
+	const keep = useSignal<number[]>([])
+	const dice = useSignal<number[]>([])
+	const checked = useSignal<number[]>([])
 
-  const dieChecked = $((e: QwikChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    const idx = checked.value.indexOf(value);
-    if (idx == -1) {
-      checked.value.push(value);
-    } else {
-      checked.value.splice(idx, 1);
-    }
-    keep.value = [];
-    for (const idx of checked.value) {
-      const d = dice.value[idx];
-      keep.value.push(d);
-    }
-  });
+	const dieChecked = $((e: QwikChangeEvent<HTMLInputElement>) => {
+		const value = parseInt(e.target.value)
+		const idx = checked.value.indexOf(value)
+		if (idx == -1) {
+			checked.value.push(value)
+		} else {
+			checked.value.splice(idx, 1)
+		}
+		keep.value = []
+		for (const idx of checked.value) {
+			const d = dice.value[idx]
+			keep.value.push(d)
+		}
+	})
 
-  const rollClicked = $(() => {
-    props.rollDice(keep.value);
-  });
+	const rollClicked = $(() => {
+		props.rollDice(keep.value)
+	})
 
-  useTask$(async () => {
-    const { roll } = props;
-    dice.value = [];
-    checked.value = [];
-    for (const d of roll.split(",")) {
-      if (d) dice.value.push(parseInt(d));
-    }
-  });
-  return (
-    <div>
-      <h1>{heading}</h1>
-      <div class="flex flex-wrap">
-        {dice.value.map((d, i) => (
-          <div key={i} class="mr-4">
-            <div>{d.toString()}</div>
-            <input
-              type="checkbox"
-              name={"die-" + i}
-              value={i}
-              onChange$={dieChecked}
-              disabled={disabled}
-            />
-          </div>
-        ))}
-      </div>
-      <button onClick$={rollClicked} disabled={disabled}>
-        {btnLabel}
-      </button>
-    </div>
-  );
-});
+	useTask$(async () => {
+		const { roll } = props
+		dice.value = []
+		checked.value = []
+		for (const d of roll.split(',')) {
+			if (d) dice.value.push(parseInt(d))
+		}
+	})
+	return (
+		<div class="yacht-turn-display">
+			<h1>{heading}</h1>
+			<div class="flex flex-wrap">
+				{dice.value.map((d, i) => (
+					<div key={i} class="mr-4">
+						<DieDisplayLg face={d} />
+						<div class="w-20 text-center">
+							<input
+								type="checkbox"
+								name={'die-' + i}
+								value={i}
+								onChange$={dieChecked}
+								disabled={disabled}
+							/>
+						</div>
+					</div>
+				))}
+			</div>
+			<button onClick$={rollClicked} disabled={disabled}>
+				{btnLabel}
+			</button>
+		</div>
+	)
+})
